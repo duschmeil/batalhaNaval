09c3a0946e08c2e53dafd29d2ba812c2a9d33da1
eduschmeil <eduschmeil@gmail.com>
2025-06-03 13:06:32 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from time import sleep
+from random import randint
+
+matrizJogador = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+matrizComputador = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def printTabuleiro(matriz):
+    cont = 0
+    print("      1  2  3  4  5  6  7  8  9  10")
+    print("      -----------------------------")
+    for linha in matriz:
+        cont += 1
+        print(f"{cont:2} | {linha}")
+    
+print("TABULEIRO DO JOGADOR")
+printTabuleiro(matrizJogador)
+
+destroierPLinha, destroierPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU DESTROIER: [LINHA COLUNA] ").split())
+matrizJogador[destroierPColuna - 1][destroierPLinha - 1] = 'D'
+
+printTabuleiro(matrizJogador)
