7e6af5d01f4764739886b86a6b56035854d691a8
eduschmeil <eduschmeil@gmail.com>
2025-06-03 21:51:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from time import sleep
 from random import randint
 
+RESET = '\033[0m'
+VERMELHO = '\033[91m'
+AZUL = '\033[94m'
+VERDE = '\033[92m'
+AMARELO = '\033[93m'
+ROXO = '\033[95m'
+BRANCO = '\033[97m'
+ROSA = '\033[35m'
+
+
+def colorir(celula):
+    if celula == 'D':
+        return f"{AMARELO}D{RESET}"
+    elif celula == 'T':
+        return f"{VERDE}T{RESET}"
+    elif celula == 'C':
+        return f"{ROXO}C{RESET}"
+    elif celula == 'S':
+        return f"{ROSA}S{RESET}"
+    elif celula == 'P':
+        return f"{BRANCO}P{RESET}"
+    elif celula == 'X':
+        return f"{VERMELHO}X{RESET}"
+    elif celula == '0':
+        return f"{AZUL}O{RESET}"
+    else:
+        return f"{celula}"
+
+
 matrizJogador = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -25,16 +54,72 @@ matrizComputador = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 def printTabuleiro(matriz):
     cont = 0
-    print("      1  2  3  4  5  6  7  8  9  10")
-    print("      -----------------------------")
+    print("    1 2 3 4 5 6 7 8 9 10")
+    print("    --------------------")
     for linha in matriz:
         cont += 1
-        print(f"{cont:2} | {linha}")
+        print(f"{cont:2} -", " ".join(colorir(str(item)) for item in linha))
     
 print("TABULEIRO DO JOGADOR")
 printTabuleiro(matrizJogador)
 
-destroierPLinha, destroierPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU DESTROIER: [LINHA COLUNA] ").split())
-matrizJogador[destroierPColuna - 1][destroierPLinha - 1] = 'D'
+print()
+destroierPLinha, destroierPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU DESTROIER (1x1): [LINHA COLUNA] ").split())
+matrizJogador[destroierPLinha - 1][destroierPColuna - 1] = 'D'
+
+printTabuleiro(matrizJogador)
+
+cont = 0
+while True:
+    print()
+    subPLinha, subPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU SUBMARINO (2x1): [LINHA COLUNA] ").split())
+    if matrizJogador[subPLinha - 1][subPColuna - 1] != 0 :
+        print("Já tem um navio nessa posição!")
+    else:
+        matrizJogador[subPLinha - 1][subPColuna - 1] = 'S'
+        cont += 1
+        printTabuleiro(matrizJogador)
+    if cont == 2:
+        break
+
+cont = 0
+while True:
+    print()
+    ctPLinha, ctPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU CONTRATORPEDEIRO (3x1): [LINHA COLUNA] ").split())
+    if matrizJogador[ctPLinha - 1][ctPColuna - 1] != 0 :
+        print("Já tem um navio nessa posição!")
+    else:
+        matrizJogador[ctPLinha - 1][ctPColuna - 1] = 'C'
+        cont += 1
+        printTabuleiro(matrizJogador)
+    if cont == 3:
+        break
+
+
+cont = 0
+while True:
+    print()
+    tanquePLinha, tanquePColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU NAVIO TANQUE (4x1): [LINHA COLUNA] ").split())
+    if matrizJogador[tanquePLinha - 1][tanquePColuna - 1] != 0 :
+        print("Já tem um navio nessa posição!")
+    else:
+        matrizJogador[tanquePLinha - 1][tanquePColuna - 1] = 'T'
+        cont += 1
+        printTabuleiro(matrizJogador)
+    if cont == 4:
+        break
+
+cont = 0
+while True:
+    print()
+    paPLinha, paPColuna = map(int, input("SELECIONE A POSIÇÃO DO SEU PORTA-AVIÕES (5x1): [LINHA COLUNA] ").split())
+    if matrizJogador[paPLinha - 1][paPColuna - 1] != 0 :
+        print("Já tem um navio nessa posição!")
+    else:
+        matrizJogador[paPLinha - 1][paPColuna - 1] = 'P'
+        cont += 1
+        printTabuleiro(matrizJogador)
+    if cont == 5:
+        break
 
 printTabuleiro(matrizJogador)
