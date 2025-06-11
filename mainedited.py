aa61f3c53b13cbec049026a7a49754069cfa9926
barbozapbernardo <clashcomtudo@gmail.com>
2025-06-11 13:02:43 -0300

    edit navios destruidos

--- mainedited.py
+++ mainedited.py
@@ -287,7 +287,7 @@ def jogar_batalha_naval():
             print("\nSUA VEZ DE ATACAR!")
             sleep(0.5)
             jogadaJogador(matrizComputador)
-            checkNaviosDestruidos(matrizJogador)
+            checkNaviosDestruidos(matrizComputador)
             if todos_navios_afundados(matrizComputador):
                 print("\nPARABÉNS! VOCÊ DESTRUIU TODOS OS NAVIOS INIMIGOS E VENCEU A BATALHA!")
                 break
